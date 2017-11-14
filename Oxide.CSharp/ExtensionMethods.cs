287a85867077f661d5a149d18ec62885094526c9
Luke Spragg <luke@spragg.me>
2017-11-13 23:47:40 -0500

    Initial commit of split/migrated base

---
+++ Oxide.CSharp/ExtensionMethods.cs
@@ -0,0 +1,9 @@
+﻿namespace Oxide.Plugins
+{
+    /// <summary>
+    /// Useful extension methods which are added to base types
+    /// </summary>
+    public static class ExtensionMethods
+    {
+    }
+}
