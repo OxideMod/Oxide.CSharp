4afb91504b14cd378cbe6513146b008809a958a3
Austin Vandersluis <austinv900@nindroidservers.com>
2023-03-19 07:40:36 -0700

    Fixed CustomAttribute Not Importing
    Added attempt to manual resolve any references outside of the plugin class.

--- src/AssemblyResolver.cs
+++ src/AssemblyResolver.cs
@@ -2,14 +2,30 @@
 
 using Oxide.Core;
 using References::Mono.Cecil;
+using System.IO;
 
 namespace Oxide.CSharp
 {
     internal class AssemblyResolver : DefaultAssemblyResolver
     {
+        internal readonly AssemblyDefinition mscorlib;
+
         public AssemblyResolver() : base()
         {
             AddSearchDirectory(Interface.Oxide.ExtensionDirectory);
+            mscorlib = AssemblyDefinition.ReadAssembly(Path.Combine(Interface.Oxide.ExtensionDirectory, "mscorlib.dll"));
+            AddSearchDirectory(CompilerService.runtimePath);
+        }
+
+        public override AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters)
+        {
+            if (name.Name == "System.Private.CoreLib")
+            {
+                Interface.Oxide.RootLogger.WriteDebug(Core.Logging.LogType.Warning, new Logging.LogEvent(50, "Resolve"), "Resolver", "Redirecting reference to System.Private.CoreLib to mscorlib");
+                return mscorlib;
+            }
+
+            return base.Resolve(name, parameters);
         }
     }
 }
