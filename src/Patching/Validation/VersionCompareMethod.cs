ea33de6eb672740bdd45b847fcc02ef824ca64d1
Austin Vandersluis <austinv900@nindroidservers.com>
2023-04-11 23:41:11 -0700

    Move VersionCompareMethod to validation namespace

---
+++ src/Patching/Validation/VersionCompareMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Oxide.CSharp.Patching.Validation
+{
+    [Flags]
+    public enum VersionCompareMethod
+    {
+        Equality = 0x00,
+        GreaterThan = 0x01,
+        LessThan = 0x02,
+        GreaterThanOrEqualTo = 0x04 | Equality | GreaterThan,
+        LessThanOrEqualTo = 0x08 | Equality | LessThan
+    }
+}
