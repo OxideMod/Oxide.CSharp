ad602dd46240618c938fc4e603dfb223a3d6429d
Austin Vandersluis <austinv900@nindroidservers.com>
2024-10-09 00:47:17 -0500

    Refactor CompilerService to use MessageBroker [skip ci]
    Significant changes include:
    - Added new dependencies in CompilerService
    - Replaced ObjectStreamClient with MessageBroker for handling compiler messages
    - Updated methods to use the new MessageBroker instance instead of the old client
    - Modified ArrayPool usage to utilize shared pool
    - Created new classes, CompilerMessage and MessageType, under a new namespace 'CompilerStream'
    - Updated package reference version in Oxide.CSharp.csproj file
    
    This is not a finished commit!

---
+++ src/CompilerStream/MessageType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Oxide.CSharp.CompilerStream
+{
+    [Flags]
+    public enum MessageType : byte
+    {
+        Unknown = 0x00,
+
+        Acknowledge = 0x01,
+
+        Heartbeat = 0x02,
+
+        VersionInfo = 0x04,
+
+        Ready = 0x08,
+
+        Command = 0x16,
+
+        Data = 0x32,
+
+        Shutdown = 0x64
+    }
+}
